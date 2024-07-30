e8ab7afc4938f43ed5e283b7986f0c18001e5713
WARDA BIBI <101542862+WardaBibi@users.noreply.github.com>
2024-07-30 11:55:07 +0500

    Update minSubArr-SlidinWindow.py

--- python/minSubArr-SlidinWindow.py
+++ python/minSubArr-SlidinWindow.py
@@ -25,8 +25,35 @@ class Solution:
         if length==float('inf'):
             return 0
         else:
-            return length                
+            return length           
 
 
+
+# ---------------------------------------------------------2nd attempt----------------------------------
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        # for i in range(1,len(nums)+1):
+        #     for j in range(len(nums)-i+1):
+        #         substr=nums[j:j+i]
+        #         # print(f' i is {i} and j is {j}')
+        #         # print(substr)
+        #         if sum(substr) >= target:
+        #             return len(substr)
+        # return 0        
+
+        l=0
+        total=0
+        minLength=float('inf')
+        for r in range(len(nums)):
+            total=total+nums[r]
+            # print(f'r is {r} l is {l} and total is {total}')
+            while total >= target:
+                minLength=min(r-l+1,minLength)
+                # print(f'---------------------r is {r} l is {l} and total is {total}')
+                total-=nums[l]
+                l+=1
+        if minLength == float('inf'):
+            return 0
+        return minLength
 
 
